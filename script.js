84f6bb624ad844ba5f2a8b54c59f428d90d470bc
HuzaifaSiddique01 <itshuzaifa09@gmail.com>
2024-02-16 01:57:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var marks = +prompt("Enter your marks");
+
+if (marks >= 90) {
+    document.write("You got A1");
+} else if (marks >= 80) {
+    document.write("You got A");
+} else if (marks >= 60) {
+    document.write("You are passed");
+} else {
+    document.write("You are failed nikammay");
+}
